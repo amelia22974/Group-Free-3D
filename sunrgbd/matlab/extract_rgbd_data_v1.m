bdacbc76d0c3bf7204fd6653f2daee193ca31eb3
amelia22974 <amelia.a.woodward@gmail.cpm>
2021-05-24 18:19:21 -0700

    Update matlab scripts so they actually run

--- sunrgbd/matlab/extract_rgbd_data_v1.m
+++ sunrgbd/matlab/extract_rgbd_data_v1.m
@@ -22,34 +22,34 @@ load('../OFFICIAL_SUNRGBD/SUNRGBDtoolbox/Metadata/SUNRGBDMeta.mat')
 det_label_folder = '../sunrgbd_trainval/label_v1/';
 mkdir(det_label_folder);
 %% Read
-for imageId = 1:10335
+parfor imageId = 1:10335
     imageId
-try
-data = SUNRGBDMeta(imageId);
-data.depthpath(1:16) = '';
-data.depthpath = strcat('../OFFICIAL_SUNRGBD/SUNRGBD', data.depthpath);
-data.rgbpath(1:16) = '';
-data.rgbpath = strcat('../OFFICIAL_SUNRGBD/SUNRGBD', data.rgbpath);
+    
+    data = SUNRGBDMeta(imageId);
+    data.depthpath(1:16) = '';
+    data.depthpath = strcat('../OFFICIAL_SUNRGBD/SUNRGBD', data.depthpath);
+    data.rgbpath(1:16) = '';
+    data.rgbpath = strcat('../OFFICIAL_SUNRGBD/SUNRGBD', data.rgbpath);
 
-% MAT files are 3x smaller than TXT files. In Python we can use
-% scipy.io.loadmat('xxx.mat')['points3d_rgb'] to load the data.
-mat_filename = strcat(num2str(imageId,'%06d'), '.mat');
-txt_filename = strcat(num2str(imageId,'%06d'), '.txt');
+    % MAT files are 3x smaller than TXT files. In Python we can use
+    % scipy.io.loadmat('xxx.mat')['points3d_rgb'] to load the data.
+    mat_filename = strcat(num2str(imageId,'%06d'), '.mat');
+    txt_filename = strcat(num2str(imageId,'%06d'), '.txt');
 
-% Write 2D and 3D box label
-data2d = data;
-fid = fopen(strcat(det_label_folder, txt_filename), 'w');
-for j = 1:length(data.groundtruth3DBB)
-    centroid = data.groundtruth3DBB(j).centroid;
-    classname = data.groundtruth3DBB(j).classname;
-    orientation = data.groundtruth3DBB(j).orientation;
-    coeffs = abs(data.groundtruth3DBB(j).coeffs);
-    box2d = data2d.groundtruth2DBB(j).gtBb2D;
-    fprintf(fid, '%s %d %d %d %d %f %f %f %f %f %f %f %f\n', classname, box2d(1), box2d(2), box2d(3), box2d(4), centroid(1), centroid(2), centroid(3), coeffs(1), coeffs(2), coeffs(3), orientation(1), orientation(2));
-end
-fclose(fid);
+    % Write 2D and 3D box label
+    data2d = data;
+    fid = fopen(strcat(det_label_folder, txt_filename), 'w');
+    for j = 1:length(data.groundtruth3DBB)
+        centroid = data.groundtruth3DBB(j).centroid;
+        classname = data.groundtruth3DBB(j).classname;
+        orientation = data.groundtruth3DBB(j).orientation;
+        coeffs = abs(data.groundtruth3DBB(j).coeffs);
+        box2d = data2d.groundtruth2DBB(j).gtBb2D;
+        fprintf(fid, '%s %d %d %d %d %f %f %f %f %f %f %f %f\n', classname, box2d(1), box2d(2), box2d(3), box2d(4), centroid(1), centroid(2), centroid(3), coeffs(1), coeffs(2), coeffs(3), orientation(1), orientation(2));
+    end
+    fclose(fid);
 
-catch
-end
+
+
 
 end
--- sunrgbd/matlab/extract_rgbd_data_v2.m
+++ sunrgbd/matlab/extract_rgbd_data_v2.m
@@ -36,48 +36,49 @@ mkdir(seg_label_folder);
 %% Read
 parfor imageId = 1:10335
     imageId
-try
-data = SUNRGBDMeta(imageId);
-data.depthpath(1:16) = '';
-data.depthpath = strcat('../OFFICIAL_SUNRGBD', data.depthpath);
-data.rgbpath(1:16) = '';
-data.rgbpath = strcat('../OFFICIAL_SUNRGBD', data.rgbpath);
+    try
+        data = SUNRGBDMeta(imageId);
+        print(data)
+        data.depthpath(1:16) = '';
+        data.depthpath = strcat('../OFFICIAL_SUNRGBD', data.depthpath);
+        data.rgbpath(1:16) = '';
+        data.rgbpath = strcat('../OFFICIAL_SUNRGBD', data.rgbpath);
 
-% Write point cloud in depth map
-[rgb,points3d,depthInpaint,imsize]=read3dPoints(data);
-rgb(isnan(points3d(:,1)),:) = [];
-points3d(isnan(points3d(:,1)),:) = [];
-points3d_rgb = [points3d, rgb];
+        % Write point cloud in depth map
+        [rgb,points3d,depthInpaint,imsize]=read3dPoints(data);
+        rgb(isnan(points3d(:,1)),:) = [];
+        points3d(isnan(points3d(:,1)),:) = [];
+        points3d_rgb = [points3d, rgb];
 
-% MAT files are 3x smaller than TXT files. In Python we can use
-% scipy.io.loadmat('xxx.mat')['points3d_rgb'] to load the data.
-mat_filename = strcat(num2str(imageId,'%06d'), '.mat');
-txt_filename = strcat(num2str(imageId,'%06d'), '.txt');
-parsave(strcat(depth_folder, mat_filename), points3d_rgb);
+        % MAT files are 3x smaller than TXT files. In Python we can use
+        % scipy.io.loadmat('xxx.mat')['points3d_rgb'] to load the data.
+        mat_filename = strcat(num2str(imageId,'%06d'), '.mat');
+        txt_filename = strcat(num2str(imageId,'%06d'), '.txt');
+        parsave(strcat(depth_folder, mat_filename), points3d_rgb);
 
-% Write images
-copyfile(data.rgbpath, sprintf('%s/%06d.jpg', image_folder, imageId));
+        % Write images
+        copyfile(data.rgbpath, sprintf('%s/%06d.jpg', image_folder, imageId));
 
-% Write calibration
-dlmwrite(strcat(calib_folder, txt_filename), data.Rtilt(:)', 'delimiter', ' ');
-dlmwrite(strcat(calib_folder, txt_filename), data.K(:)', 'delimiter', ' ', '-append');
+        % Write calibration
+        dlmwrite(strcat(calib_folder, txt_filename), data.Rtilt(:)', 'delimiter', ' ');
+        dlmwrite(strcat(calib_folder, txt_filename), data.K(:)', 'delimiter', ' ', '-append');
 
-% Write 2D and 3D box label
-data2d = SUNRGBDMeta2DBB(imageId);
-fid = fopen(strcat(det_label_folder, txt_filename), 'w');
-for j = 1:length(data.groundtruth3DBB)
-    centroid = data.groundtruth3DBB(j).centroid;
-    classname = data.groundtruth3DBB(j).classname;
-    orientation = data.groundtruth3DBB(j).orientation;
-    coeffs = abs(data.groundtruth3DBB(j).coeffs);
-    box2d = data2d.groundtruth2DBB(j).gtBb2D;
-    assert(strcmp(data2d.groundtruth2DBB(j).classname, classname));
-    fprintf(fid, '%s %d %d %d %d %f %f %f %f %f %f %f %f\n', classname, box2d(1), box2d(2), box2d(3), box2d(4), centroid(1), centroid(2), centroid(3), coeffs(1), coeffs(2), coeffs(3), orientation(1), orientation(2));
-end
-fclose(fid);
+        % Write 2D and 3D box label
+        data2d = SUNRGBDMeta2DBB(imageId);
+        fid = fopen(strcat(det_label_folder, txt_filename), 'w');
+        for j = 1:length(data.groundtruth3DBB)
+            centroid = data.groundtruth3DBB(j).centroid;
+            classname = data.groundtruth3DBB(j).classname;
+            orientation = data.groundtruth3DBB(j).orientation;
+            coeffs = abs(data.groundtruth3DBB(j).coeffs);
+            box2d = data2d.groundtruth2DBB(j).gtBb2D;
+            assert(strcmp(data2d.groundtruth2DBB(j).classname, classname));
+            fprintf(fid, '%s %d %d %d %d %f %f %f %f %f %f %f %f\n', classname, box2d(1), box2d(2), box2d(3), box2d(4), centroid(1), centroid(2), centroid(3), coeffs(1), coeffs(2), coeffs(3), orientation(1), orientation(2));
+        end
+        fclose(fid);
 
-catch
-end
+        catch
+        end
 
 end
 
